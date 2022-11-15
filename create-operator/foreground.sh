06136f3e5a88c364466332439a51d19be4badd05
chadmcrowell <chadmcrowell@gmail.com>
2022-11-15 10:25:14 -0600

    delete tar && make working directory

--- create-operator/foreground.sh
+++ create-operator/foreground.sh
@@ -6,3 +6,8 @@ chmod +x kubebuilder && mv kubebuilder /usr/local/bin/
 wget https://go.dev/dl/go1.19.3.linux-amd64.tar.gz
 rm -rf /usr/local/go && tar -C /usr/local -xzf go1.19.3.linux-amd64.tar.gz
 go version
+rm go1.19.3.linux-amd64.tar.gz
+
+# make working directory
+mkdir -p src
+cd ./src
