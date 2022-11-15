6f76b9b8f2bb9298b0e5808da28749090a3120f5
chadmcrowell <chadmcrowell@gmail.com>
2022-11-15 10:21:08 -0600

    update go in foreground script

--- create-operator/foreground.sh
+++ create-operator/foreground.sh
@@ -1,3 +1,10 @@
 # download kubebuilder and install locally.
 curl -L -o kubebuilder https://go.kubebuilder.io/dl/latest/$(go env GOOS)/$(go env GOARCH)
 chmod +x kubebuilder && mv kubebuilder /usr/local/bin/
+
+# update go
+wget https://go.dev/dl/go1.19.3.linux-amd64.tar.gz
+rm -rf /usr/local/go && tar -C /usr/local -xzf go1.19.3.linux-amd64.tar.gz
+go version
+
+echo "********HAPPY BUILDING***************"
