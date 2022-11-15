8ae7dc0b1e5aabbd9c58fa06a2f3e9682f1030d6
chadmcrowell <chadmcrowell@gmail.com>
2022-11-15 10:22:48 -0600

    delete echo

--- create-operator/foreground.sh
+++ create-operator/foreground.sh
@@ -6,5 +6,3 @@ chmod +x kubebuilder && mv kubebuilder /usr/local/bin/
 wget https://go.dev/dl/go1.19.3.linux-amd64.tar.gz
 rm -rf /usr/local/go && tar -C /usr/local -xzf go1.19.3.linux-amd64.tar.gz
 go version
-
-echo "********HAPPY BUILDING***************"
