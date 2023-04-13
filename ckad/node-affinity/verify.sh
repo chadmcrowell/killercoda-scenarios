93b59c2c12b058d4a64f767c4eecf2ec956d6235
Chad Crowell <chadmcrowell@gmail.com>
2023-04-13 17:02:34 -0500

    fixed verify

--- ckad/node-affinity/verify.sh
+++ ckad/node-affinity/verify.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-kubectl get po --field-selector spec.nodeName=controlplane -l 'app=az1-pod'
+# kubectl get po --field-selector spec.nodeName=controlplane -l 'app=az1-pod'
+
+kubectl get po -o wide | grep controlplane
