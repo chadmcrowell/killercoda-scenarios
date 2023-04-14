33fe045e7f2d63c407b8cf287f79204ba4660682
Chad Crowell <chadmcrowell@gmail.com>
2023-04-14 08:43:15 -0500

    added namespace

--- ckad/node-affinity/verify.sh
+++ ckad/node-affinity/verify.sh
@@ -2,4 +2,8 @@
 
 # kubectl get po --field-selector spec.nodeName=controlplane -l 'app=az1-pod'
 
+# for node in $(kubectl get nodes -l availability-zone=zone1 -ojsonpath='{.items[*].metadata.name}'); do kubectl get pods -A -owide --field-selector spec.nodeName=$node; done
+
 kubectl get po -o wide | grep controlplane
+
+kubectl -n 012963bd get po az1-pod
