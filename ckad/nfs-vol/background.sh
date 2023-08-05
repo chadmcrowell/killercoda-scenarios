14783273baf18a998f660a92cd46ac0d7bdacc23
Chad Crowell <chadmcrowell@gmail.com>
2023-08-05 15:14:06 -0500

    added to background script

--- ckad/nfs-vol/background.sh
+++ ckad/nfs-vol/background.sh
@@ -1,3 +1,7 @@
+wget https://raw.githubusercontent.com/chadmcrowell/acing-the-ckad-exam/main/ch_02/pod-nfs-vol.yaml
+
 wget https://raw.githubusercontent.com/chadmcrowell/acing-the-ckad-exam/main/ch_02/nfs-server-install.sh
 
 chmod +x ./nfs-server-install.sh
+
+scp ./nfs-server-install.sh node01:~/
