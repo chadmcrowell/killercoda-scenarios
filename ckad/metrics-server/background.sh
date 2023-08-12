9f92603dc3b1c9827cef569c2b11233e2bd638d5
Chad Crowell <chadmcrowell@gmail.com>
2023-08-12 10:51:08 -0500

    changed background script

--- ckad/metrics-server/background.sh
+++ ckad/metrics-server/background.sh
@@ -4,8 +4,8 @@ kubectl apply -f https://raw.githubusercontent.com/chadmcrowell/acing-the-ckad-e
 
 # create pods
 
+kubectl apply -f https://raw.githubusercontent.com/chadmcrowell/acing-the-ckad-exam/main/ch_02/pod-php-apache.yaml
+
 kubectl apply -f https://raw.githubusercontent.com/chadmcrowell/acing-the-ckad-exam/main/ch_02/pod-emptydir-two-containers-full.yaml
 
 kubectl apply -f https://raw.githubusercontent.com/chadmcrowell/acing-the-ckad-exam/main/ch_02/pod-hostpath.yaml
-
-kubectl apply -f https://raw.githubusercontent.com/chadmcrowell/acing-the-ckad-exam/main/ch_02/pod-php-apache.yaml
