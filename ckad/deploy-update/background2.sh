0e992075ee19c9e34b6cc88ebea49953871a752e
Chad Crowell <chadmcrowell@gmail.com>
2023-09-03 12:52:36 -0500

    update the background scripts

--- ckad/deploy-update/background2.sh
+++ ckad/deploy-update/background2.sh
@@ -1,3 +1,3 @@
-k -n session283884 expose deploy scaler --port 80 --type NodePort --dry-run=client -o yaml > svc.yaml
+kubectl -n session283884 expose deploy scaler --port 80 --type NodePort --dry-run=client -o yaml > svc.yaml
 
-k apply -f svc.yaml
+kubectl apply -f svc.yaml
