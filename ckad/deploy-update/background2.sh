547955debc74492549bc2bab89f62b26d99c8f2f
Chad Crowell <chadmcrowell@gmail.com>
2023-09-03 13:00:49 -0500

    update the background scripts insert sed

--- ckad/deploy-update/background2.sh
+++ ckad/deploy-update/background2.sh
@@ -1,3 +1,5 @@
 kubectl -n session283884 expose deploy scaler --port 80 --type NodePort --dry-run=client -o yaml > svc.yaml
 
+sed -i 's|targetPort: 80|nodePort: 30942|g' ./svc.yaml
+
 kubectl apply -f ./svc.yaml
