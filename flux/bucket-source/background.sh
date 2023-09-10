7b277f7f8b792ef53f6f73e93bbd0f3dea961641
Chad Crowell <chadmcrowell@gmail.com>
2023-09-10 10:16:29 -0500

    added flux install script

--- flux/bucket-source/background.sh
+++ flux/bucket-source/background.sh
@@ -1,7 +1,7 @@
 curl -s https://fluxcd.io/install.sh | sudo bash
 
-flux install
-
 wget https://raw.githubusercontent.com/chadmcrowell/flux-playground/main/bucket-source/bucket-policy.json
 
 wget https://raw.githubusercontent.com/chadmcrowell/flux-playground/main/bucket-source/inline-policy.json
+
+flux install
