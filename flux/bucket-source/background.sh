db21be8dec20300849b8168f4676d60521e9d359
Chad Crowell <chadmcrowell@gmail.com>
2023-09-10 10:22:54 -0500

    added aws cli install script

--- flux/bucket-source/background.sh
+++ flux/bucket-source/background.sh
@@ -5,3 +5,10 @@ wget https://raw.githubusercontent.com/chadmcrowell/flux-playground/main/bucket-
 wget https://raw.githubusercontent.com/chadmcrowell/flux-playground/main/bucket-source/inline-policy.json
 
 flux install
+
+
+curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+
+unzip awscliv2.zip
+
+sudo ./aws/install
