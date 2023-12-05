a8384b34abcffdfa01c3babb7cd05f386450bf20
Chad Crowell <chadmcrowell@gmail.com>
2023-12-05 09:17:34 -0600

    added job yaml in background script

--- ckad/jobs/background.sh
+++ ckad/jobs/background.sh
@@ -1,2 +1,4 @@
 wget https://raw.githubusercontent.com/chadmcrowell/acing-the-ckad-exam/main/ch_02/cronjob.yaml
 
+wget https://raw.githubusercontent.com/chadmcrowell/acing-the-ckad-exam/main/ch_02/job-countdown.yaml
+
