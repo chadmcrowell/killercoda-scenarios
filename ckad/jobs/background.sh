ea1a2ea190dce985361d670f8e1d62a7f321cf21
Chad Crowell <chadmcrowell@gmail.com>
2023-12-05 09:23:00 -0600

    updated background script

--- ckad/jobs/background.sh
+++ ckad/jobs/background.sh
@@ -1,4 +1,3 @@
-wget https://raw.githubusercontent.com/chadmcrowell/acing-the-ckad-exam/main/ch_02/cronjob.yaml
-
 wget https://raw.githubusercontent.com/chadmcrowell/acing-the-ckad-exam/main/ch_02/job-countdown.yaml
 
+wget https://raw.githubusercontent.com/chadmcrowell/acing-the-ckad-exam/main/ch_02/cronjob.yaml
