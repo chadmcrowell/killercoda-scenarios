da3d3a29722151967f08b3391bd886935433f3ec
chad <chad@civo.com>
2024-08-09 12:18:03 -0500

    fix prometheus background script

--- prometheus/background.sh
+++ prometheus/background.sh
@@ -6,37 +6,40 @@ helm install prometheus prometheus-community/prometheus
 
 sleep 30
 
-cat <<EOF | kubectl apply -f -
-apiVersion: v1
-kind: Service
-metadata:
-  annotations:
-    meta.helm.sh/release-name: prometheus
-    meta.helm.sh/release-namespace: default
-  labels:
-    app.kubernetes.io/component: server
-    app.kubernetes.io/instance: prometheus
-    app.kubernetes.io/managed-by: Helm
-    app.kubernetes.io/name: prometheus
-    app.kubernetes.io/part-of: prometheus
-    app.kubernetes.io/version: v2.53.1
-    helm.sh/chart: prometheus-25.25.0
-  name: prometheus-server
-  namespace: default
-spec:
-  ipFamilies:
-  - IPv4
-  ipFamilyPolicy: SingleStack
-  ports:
-  - name: http
-    nodePort: 30000
-    port: 80
-    protocol: TCP
-    targetPort: 9090
-  selector:
-    app.kubernetes.io/component: server
-    app.kubernetes.io/instance: prometheus
-    app.kubernetes.io/name: prometheus
-  sessionAffinity: None
-  type: NodePort
-EOF
+kubectl patch svc prometheus-server -p '{"spec": {"type": "NodePort", "ports": [{"port": 80, "nodePort": 30000}]}}'
+
+
+# cat <<EOF | kubectl apply -f -
+# apiVersion: v1
+# kind: Service
+# metadata:
+#   annotations:
+#     meta.helm.sh/release-name: prometheus
+#     meta.helm.sh/release-namespace: default
+#   labels:
+#     app.kubernetes.io/component: server
+#     app.kubernetes.io/instance: prometheus
+#     app.kubernetes.io/managed-by: Helm
+#     app.kubernetes.io/name: prometheus
+#     app.kubernetes.io/part-of: prometheus
+#     app.kubernetes.io/version: v2.53.1
+#     helm.sh/chart: prometheus-25.25.0
+#   name: prometheus-server
+#   namespace: default
+# spec:
+#   ipFamilies:
+#   - IPv4
+#   ipFamilyPolicy: SingleStack
+#   ports:
+#   - name: http
+#     nodePort: 30000
+#     port: 80
+#     protocol: TCP
+#     targetPort: 9090
+#   selector:
+#     app.kubernetes.io/component: server
+#     app.kubernetes.io/instance: prometheus
+#     app.kubernetes.io/name: prometheus
+#   sessionAffinity: None
+#   type: NodePort
+# EOF
