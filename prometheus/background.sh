25a7b576a4ce5adaa4ff1ed37b2f1cfb1560e6ff
chad <chad@civo.com>
2024-08-09 12:12:47 -0500

    setup prometheus scenario 2

--- prometheus/background.sh
+++ prometheus/background.sh
@@ -3,3 +3,40 @@ helm repo add prometheus-community https://prometheus-community.github.io/helm-c
 helm repo update
 
 helm install prometheus prometheus-community/prometheus
+
+sleep 30
+
+cat <<EOF | kubectl apply -f -
+apiVersion: v1
+kind: Service
+metadata:
+  annotations:
+    meta.helm.sh/release-name: prometheus
+    meta.helm.sh/release-namespace: default
+  labels:
+    app.kubernetes.io/component: server
+    app.kubernetes.io/instance: prometheus
+    app.kubernetes.io/managed-by: Helm
+    app.kubernetes.io/name: prometheus
+    app.kubernetes.io/part-of: prometheus
+    app.kubernetes.io/version: v2.53.1
+    helm.sh/chart: prometheus-25.25.0
+  name: prometheus-server
+  namespace: default
+spec:
+  ipFamilies:
+  - IPv4
+  ipFamilyPolicy: SingleStack
+  ports:
+  - name: http
+    nodePort: 30000
+    port: 80
+    protocol: TCP
+    targetPort: 9090
+  selector:
+    app.kubernetes.io/component: server
+    app.kubernetes.io/instance: prometheus
+    app.kubernetes.io/name: prometheus
+  sessionAffinity: None
+  type: NodePort
+EOF
--- prometheus/step1.md
+++ prometheus/step1.md
@@ -1,4 +1,4 @@
-[Click here to open the Prometheus Dashboard in a new tab]({{TRAFFIC_HOST1_9090}})
+[Click here to open the Prometheus Dashboard in a new tab]({{TRAFFIC_HOST1_30000}})
 
 <br>
 <details><summary>Solution</summary>
