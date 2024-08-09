8c9f1ab520cd4c0f23c541c2e2a6634e9471a363
chad <chad@civo.com>
2024-08-09 12:35:22 -0500

    more time to script to foreground

--- prometheus/foreground.sh
+++ prometheus/foreground.sh
@@ -1,3 +1,3 @@
-echo "Deploying scenario..." && sleep 5
+echo "Deploying scenario..." && sleep 10
 
 kubectl wait --for=condition=ready --timeout=300s pod -l app.kubernetes.io/name=prometheus -n default
