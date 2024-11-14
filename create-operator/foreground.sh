2d7278c8716a21d220ce21c6c7841b644aa2eaec
chad <chad@civo.com>
2024-11-14 07:59:33 -0600

    updated go

--- create-operator/foreground.sh
+++ create-operator/foreground.sh
@@ -3,8 +3,8 @@ curl -L -o kubebuilder https://go.kubebuilder.io/dl/latest/$(go env GOOS)/$(go e
 chmod +x kubebuilder && mv kubebuilder /usr/local/bin/
 
 # update go
-wget https://go.dev/dl/go1.19.3.linux-amd64.tar.gz
-rm -rf /usr/local/go && tar -C /usr/local -xzf go1.19.3.linux-amd64.tar.gz
+wget https://go.dev/dl/go1.21.0.linux-amd64.tar.gz
+rm -rf /usr/local/go && tar -C /usr/local -xzf go1.21.0.linux-amd64.tar.gz
 go version
 rm go1.19.3.linux-amd64.tar.gz
 
