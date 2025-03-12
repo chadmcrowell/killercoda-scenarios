36776f0db3d6dcf42c5e09d8ac08079b6f177f78
chadmcrowell <chadmcrowell@gmail.com>
2025-03-12 12:27:54 +0100

    added gateway CRDs and gw class in background

--- cka/install-gateway-api/background.sh
+++ cka/install-gateway-api/background.sh
@@ -1,2 +1,4 @@
+kubectl apply -f https://github.com/kubernetes-sigs/gateway-api/releases/download/v1.0.0/standard-install.yaml
+
 kubectl apply -f https://raw.githubusercontent.com/chadmcrowell/acing-the-cka-exam/refs/heads/main/ch_06/gatewayClass-simple.yaml
 
