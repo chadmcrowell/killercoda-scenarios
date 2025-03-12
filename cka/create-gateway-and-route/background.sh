42f54b37aa6a7fef3580c66d7ab9923efb4785e7
chadmcrowell <chadmcrowell@gmail.com>
2025-03-12 13:45:29 +0100

    added nginx controller install in background

--- cka/create-gateway-and-route/background.sh
+++ cka/create-gateway-and-route/background.sh
@@ -1,4 +1,8 @@
 kubectl apply -f https://github.com/kubernetes-sigs/gateway-api/releases/download/v1.2.0/standard-install.yaml
 
-kubectl apply -f https://raw.githubusercontent.com/chadmcrowell/acing-the-cka-exam/refs/heads/main/ch_06/gatewayClass-simple.yaml
+kubectl apply -f https://raw.githubusercontent.com/nginx/nginx-gateway-fabric/v1.6.2/deploy/crds.yaml
+
+kubectl apply -f https://raw.githubusercontent.com/nginx/nginx-gateway-fabric/v1.6.2/deploy/default/deploy.yaml
+
+kubectl apply -f https://raw.githubusercontent.com/chadmcrowell/acing-the-cka-exam/refs/heads/main/ch_06/gatewayClass-nginx-controller.yaml
 
