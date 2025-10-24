da4727779c62cb904c2f9a4ae68ec193c10ab120
chadmcrowell <chadmcrowell@gmail.com>
2025-10-24 12:45:02 +0200

    update go

--- create-operator/foreground.sh
+++ create-operator/foreground.sh
@@ -3,10 +3,10 @@ curl -L -o kubebuilder https://go.kubebuilder.io/dl/latest/$(go env GOOS)/$(go e
 chmod +x kubebuilder && mv kubebuilder /usr/local/bin/
 
 # update go
-wget https://go.dev/dl/go1.21.0.linux-amd64.tar.gz
-rm -rf /usr/local/go && tar -C /usr/local -xzf go1.21.0.linux-amd64.tar.gz
+wget https://go.dev/dl/go1.23.2.linux-amd64.tar.gz
+rm -rf /usr/local/go && tar -C /usr/local -xzf go1.23.2.linux-amd64.tar.gz
 go version
-rm go1.21.0.linux-amd64.tar.gz
+rm go1.23.2.linux-amd64.tar.gz
 
 # make working directory
 mkdir -p src
