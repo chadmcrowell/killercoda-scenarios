695be2a158d8df4e8ca01bc02b439d1e01bc2b3f
chadmcrowell <chadmcrowell@gmail.com>
2025-10-24 10:52:20 +0200

    updated ingress host routing scenario

--- ckad/ingress-host-routing/background.sh
+++ ckad/ingress-host-routing/background.sh
@@ -1,13 +1,5 @@
 #!/bin/sh
 set -euo pipefail
 
-kubectl create ns ingress-nginx
+kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/main/deploy/static/provider/cloud/deploy.yaml
 
-MANIFEST="https://raw.githubusercontent.com/kubernetes/ingress-nginx/main/deploy/static/provider/cloud/deploy.yaml"
-
-kubectl get namespace ingress-nginx >/dev/null 2>&1 || \
-  kubectl apply -f "$MANIFEST"
-
-kubectl apply -f "$MANIFEST"
-
-kubectl -n ingress-nginx rollout status deploy/ingress-nginx-controller --timeout=300s
--- ckad/ingress-host-routing/foreground.sh
+++ ckad/ingress-host-routing/foreground.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 set -euo pipefail
 
+sleep 4
+
 kubectl -n ingress-nginx rollout status deploy/ingress-nginx-controller --timeout=300s
 kubectl -n ingress-nginx get pods -l app.kubernetes.io/component=controller
