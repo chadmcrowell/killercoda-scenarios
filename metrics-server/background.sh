243357944d4a5b0f9a0695c479daf95ecb9a2f45
chadmcrowell <chadmcrowell@gmail.com>
2025-11-29 14:30:03 +0100

    add metrics server playground to root

---
+++ metrics-server/background.sh
@@ -0,0 +1,11 @@
+# Install Metrics Server
+
+kubectl apply -f https://raw.githubusercontent.com/chadmcrowell/acing-the-ckad-exam/main/ch_02/metrics-server-components.yaml
+
+# create pods
+
+# kubectl apply -f https://raw.githubusercontent.com/chadmcrowell/acing-the-ckad-exam/main/ch_02/pod-php-apache.yaml
+
+# kubectl apply -f https://raw.githubusercontent.com/chadmcrowell/acing-the-ckad-exam/main/ch_02/pod-emptydir-two-containers-full.yaml
+
+# kubectl apply -f https://raw.githubusercontent.com/chadmcrowell/acing-the-ckad-exam/main/ch_02/pod-hostpath.yaml
