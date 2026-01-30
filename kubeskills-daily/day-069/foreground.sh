5c33151417b3a98073269d410b51d9dc22bbe067
chadmcrowell <chadmcrowell@gmail.com>
2026-01-30 15:20:38 +0100

    moved script to foreground

--- kubeskills-daily/day-069/foreground.sh
+++ kubeskills-daily/day-069/foreground.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
 
-echo "Deploying scenario..." && sleep 10
+# Install Istio
+curl -L https://istio.io/downloadIstio | sh -
+# cd istio-*
+# export PATH=$PWD/bin:$PATH
+
+# Install with demo profile
+istioctl install --set profile=demo -y
+
+# Enable sidecar injection
+kubectl label namespace default istio-injection=enabled
